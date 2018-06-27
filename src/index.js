be624fd82d715a9943e6ce4ec963eb122e8808f0
rudenate3 <nathan.odin@gmail.com>
2018-06-26 22:48:02 -0400

    Add webpack, dependencies and config
    -(feature) Add webpack, related dependencies and add webpack config

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Require index.html so it will watch for changes
+require('../index.html');
+
+// Require Javascript files
+require('./js/app.js');
+
+// Require SCSS files
+require('./scss/styles.scss');
